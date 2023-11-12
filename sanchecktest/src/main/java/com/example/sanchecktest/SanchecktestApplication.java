835f3431373e076af5e59a2ae75ca6795cbb15ff
Parksonia <sonia.sy1992@gmail.com>
2023-11-13 02:19:53 +0900

    [create]testproject

---
+++ sanchecktest/src/main/java/com/example/sanchecktest/SanchecktestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sanchecktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanchecktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanchecktestApplication.class, args);
+	}
+
+}
---
+++ sanchecktest/src/test/java/com/example/sanchecktest/SanchecktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanchecktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanchecktestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

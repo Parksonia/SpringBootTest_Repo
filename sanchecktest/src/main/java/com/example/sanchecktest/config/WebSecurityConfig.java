87eea86a861f9fbc630573eba971a97b36ad579f
Parksonia <sonia.sy1992@gmail.com>
2023-11-15 01:15:38 +0900

    [update]security설정

--- sanchecktest/src/main/java/com/example/sanchecktest/config/WebSecurityConfig.java
+++ sanchecktest/src/main/java/com/example/sanchecktest/config/WebSecurityConfig.java
@@ -12,37 +12,44 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 @RequiredArgsConstructor  //생성자 생략 가능 의존성 자동 주입
 @Configuration
 public class WebSecurityConfig {  // 실제 인증 처리를 하는 config.java
 
   private final UserDetailService userService;
 
-    //스프링 시큐리티 기능 비활성화 - 정적 리소스(static 하위 img등의 resource),h2 console 하위 url에 설정
+/*    //스프링 시큐리티 기능 비활성화 - 정적 리소스(static 하위 img등의 resource),h2 console 하위 url에 설정
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
-                .requestMatchers("/static/**","/h2-console/**"); //static 하위 리소스
-
-    }
+                .requestMatchers("/static/**") //static 하위 리소스
+                .requestMatchers(toH2Console());
+    }*/
     //특정 HTTP요청에 대한 웹기반 보안 구성
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http
-                .authorizeHttpRequests(request -> request.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/reserveClick")).authenticated()
-                        .requestMatchers(new AntPathRequestMatcher("/reservationStatus")).authenticated()
-                        .requestMatchers(new AntPathRequestMatcher("/setting/user")).hasRole("USER").anyRequest().permitAll())
+                .authorizeHttpRequests((authorizeHttpRequests)-> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+               /* srpingsecurity 구 버전에서 사용하는 방법인듯함
+                .authorizeHttpRequests()//인증,인가
+                .requestMatchers("/login","/signup","user").permitAll()
+                .anyRequest().authenticated()
+                .and()*/
                 .formLogin() //폼 기반 로그인 설정 (커스텀,,,?)
                 .loginPage("/login")
-                .defaultSuccessUrl("/hello")
+                .defaultSuccessUrl("/")
                 .and()
                 .logout() //로그아웃설정
                 .logoutSuccessUrl("/login")
                 .invalidateHttpSession(true)  //로그아웃하고 세션 정보 삭제 여부
                 .and()
-                .csrf().disable() //csrf 비활성화
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+             /*   .csrf().disable() //csrf 비활성화*/
                 .build();
     }
 

083694d2dceffcc71ff2cc1d8a8c5a3ff82426a4
Parksonia <sonia.sy1992@gmail.com>
2023-11-17 03:48:58 +0900

    [fix] Authentication import 수정

--- sanchecktest/src/main/java/com/example/sanchecktest/config/jwt/TokenProvider.java
+++ sanchecktest/src/main/java/com/example/sanchecktest/config/jwt/TokenProvider.java
@@ -6,13 +6,12 @@ import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Set;
@@ -21,56 +20,54 @@ import java.util.Set;
 @Service
 public class TokenProvider {
 
-    private final JwtProperties jwtProperties;
+    private final JwtProperties jwtProperties; //properties 객체 정보
 
-    public String getnerateToken(User user, Duration expiredAt) {
-
+    public String generrateToken(User user, Duration expiredAt) {
         Date now = new Date();
-        return makeToken(new Date(now.getTime() + expiredAt.toMillis()),user);
+      return makeToken(new Date(now.getTime() + expiredAt.toMillis()),user);
+
     }
 
-    //토큰 생성 메서드, 파라미터로 만료시간과 유저정보를 받음
-    private String makeToken(Date expiry,User user) {
-        Date now = new Date();
+    private String makeToken(Date expiry, User user) {
+    Date now = new Date();
 
-        return Jwts.builder()
-                .setHeaderParam(Header.TYPE,Header.JWT_TYPE)//headertype:JWT
-                .setIssuer(jwtProperties.getIssuer())//내용:jwt.properties에서 설정한 값(sonia.sy1992@gmail.com)
-                .setIssuedAt(now) //내용: 현재시간
-                .setExpiration(expiry) //내용 :expiry 멤버 변수 값
-                .setSubject(user.getEmail()) //내용 : 유저 이메일
-                .claim("id",user.getId())  //클레임 id : 유저ID
-                //서명 : jwt.properites secret_key 비밀값 과 함께  해시값을 HS256 방식으로 암호화
-                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
-                .compact();
+    return Jwts.builder()
+            .setHeaderParam(Header.TYPE,Header.JWT_TYPE)
+            .setIssuer(jwtProperties.getIssuer())
+            .setIssuedAt(now)
+            .setExpiration(expiry)
+            .setSubject(user.getUserid())  // 'subjectValue'는 도메인 내 사용자의 고유 식별자
+            .claim("id",user.getId())
+            .signWith(SignatureAlgorithm.HS256,jwtProperties.getSecretKey())
+            .compact();
 
     }
-    //토큰이 유효한 지 검사하는 메서드
-    public boolean validToken(String token) {
+    public boolean validateToken(String token) {
         try {
             Jwts.parser()
-                    .setSigningKey(jwtProperties.getSecretKey()) //비밀값으로 복호화
+                    .setSigningKey(jwtProperties.getSecretKey())
                     .parseClaimsJws(token);
-            return true; // 유효
+            return  true;
         }catch (Exception e) {
-            return false;  //복호화 과정에서 에러가 난다면 유효하지 않은 토큰임
-
+            return false;
         }
-
     }
-    //토큰 기반으로 인증 정보를 가져오는 메서드
     public Authentication getAuthentication(String token) {
         Claims claims = getClaims(token);
         Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
 
-        return new UsernamePasswordAuthenticationToken(new org.springframework.security.core.userdetails.User(claims.getSubject(),"",authorities),token,authorities);
+        return new UsernamePasswordAuthenticationToken(new org.springframework.security.core.userdetails.User(
+                claims.getSubject(),"",authorities),token,authorities);
     }
 
+    public Long getUserId(String token) {
+        Claims claims =getClaims(token);
+        return claims.get("id",Long.class);
+    }
     private Claims getClaims(String token) {
-
-    return Jwts.parser() //클레임 조회
+    return Jwts.parser()
             .setSigningKey(jwtProperties.getSecretKey())
-            .parseClaimsJwt(token)
+            .parseClaimsJws(token)
             .getBody();
     }
 
